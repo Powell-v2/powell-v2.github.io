7726e5561a3deacadc84d393f6b13c1d4f735c46
Powell-v2 <oatto.paul@gmail.com>
2017-03-15 21:37:53 +0000

    - minor changes to contact form
    Toggle submit button depending on “email” field’s input

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,63 @@
-var main = function () {
+'use strict';
 
+function core() {
 	// initialize tooltip
-	$(function () {
-  	$('[data-toggle="tooltip"]').tooltip();
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
 	});
-
-};
+}
         
-$(document).ready(main);
+$(document).ready(core);
 
-// Changing Tab's Title
 
-var message = "Feeling good!";
-var original = document.title;
 
-window.onblur = function () { document.title = message; }
-window.onfocus = function () { document.title = original; }
+function Main() {
+  // shortcuts to DOM elements
+  this.emailInput = document.getElementById('email');
+  this.submitButton = document.getElementById('submit');
 
-// Custom validation message for 'email' form field
+  // toggle for the button
+  var buttonTogglingHandler = this.toggleButton.bind(this);
+  this.emailInput.addEventListener('keyup', buttonTogglingHandler);
+  this.emailInput.addEventListener('change', buttonTogglingHandler);
+  
+  var customValidationMessageHandler = this.setCustomValidationMessage.bind(this);
+  this.emailInput.addEventListener('keyup', customValidationMessageHandler);
+  this.emailInput.addEventListener('change', customValidationMessageHandler);
+}
 
-var email = document.getElementById("inputEmail");
-
-email.addEventListener("keyup", function (event) {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity("This field expects an e-mail");
+// enables or disables the submit button depending on 
+// the values of the input fields
+Main.prototype.toggleButton = function () {
+  if (this.emailInput.value) {
+    this.submitButton.removeAttribute('disabled');
   } else {
-    email.setCustomValidity("");
+    this.submitButton.setAttribute('disabled', 'true');
   }
-});
+};
+
+// custom validation message for 'email' form field
+Main.prototype.setCustomValidationMessage = function () {
+  if (this.emailInput.validity.typeMismatch) {
+    this.emailInput.setCustomValidity("This field expects an e-mail");
+  } else {
+    this.emailInput.setCustomValidity("");
+  }
+};
+
+
+// changing tab's title
+var altTitle = "Feeling good!";
+var originalTitle = document.title;
+
+window.onblur = function () {
+  document.title = altTitle;
+}
+
+window.onfocus = function () {
+  document.title = originalTitle;
+}
+
+window.onload = function () {
+  window.main = new Main();
+};

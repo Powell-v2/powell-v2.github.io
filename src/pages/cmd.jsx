6845a78e0d687770d5a4ed91ab4043a340f70f37
Paul Ermolin <oatto.paul@gmail.com>
2019-02-27 15:57:00 +0100

    refactor(terminal): add link to terminal from index

--- src/pages/cmd.jsx
+++ src/pages/cmd.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import Layout from '../components/layout'
 import SEO from '../components/seo'
 import FloatingGridCanvas from '../components/cmd/FloatingGridCanvas'
 import Terminal from '../components/cmd/Terminal'
@@ -9,13 +8,13 @@ import MainContainer from '../components/cmd/MainContainer'
 import './cmd.css'
 
 const CMD = () => (
-  <Layout>
+  <>
     <SEO title="Page two" />
     <MainContainer>
       <FloatingGridCanvas />
       <Terminal />
     </MainContainer>
-  </Layout>
+  </>
 )
 
 export default CMD

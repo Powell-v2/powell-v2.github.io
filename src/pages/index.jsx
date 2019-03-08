ceb1e12b18e6ef17499dcb17a8942ebac6f74dfc
Paul Ermolin <oatto.paul@gmail.com>
2019-03-08 17:41:56 +0100

    refactor: fade slogan into header

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,134 +1,158 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { css } from '@emotion/core'
+import React, { useState, useEffect } from 'react'
+import { css, keyframes } from '@emotion/core'
 
-import randIdx from '../utils'
-
-import './index.css'
+import { randIdx } from '../utils'
 
 const symbols = `!@#$%^&*()+-_=~<>,.?/{}[]|;:`
-const words = [`Dream.`, `Code.`, `Sleep.`, `Repeat.`]
+const words = [`Dream.`, `Code.`, `8`, `Sleep.`, `Repeat.`]
 
-const container = css`
+const HEADER_TIMING = 2
+
+const containerCss = css`
   display: grid;
   height: 100vh;
+  grid-template-rows: 1fr 7fr;
   background-color: black;
 `
 
-const header = css`
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`
+
+const headerCss = css`
   display: grid;
-  height: 100vh;
   border: 1px solid blue;
   color: gold;
-  transition: all 2s ease-in-out;
+  opacity: 1;
+  animation: ${fadeIn} ${HEADER_TIMING}s ease-in-out;
 `
 
-const infinity = css`
-  position: absolute;
-  color: firebrick;
-  left: 43.5%;
-  font-size: 4rem;
-  margin-top: -50%;
-  transition: all 2s ease-in-out;
-`
-
-const undecoded = css`
+const sloganCss = css`
+  display: grid;
+  grid-template-columns: repeat(5, max-content);
   margin: auto;
-  font-size: 10rem;
+  font-size: 3rem;
   font-weight: 700;
   text-transform: uppercase;
-  letter-spacing: 4rem;
+  letter-spacing: .5rem;
+  & li {
+    margin-top: 0;
+    transition: all 2s;
+    /* infinity sign */
+    &:nth-of-type(3) {
+      text-shadow: none;
+      color: firebrick;
+      padding: 0 4rem;
+    }
+  }
 `
 
+const flicker = keyframes`
+  0%, 8.999%, 16%, 31.999%, 47%, 69.999%, 73%, 74.999%, 76%, 83.999%, 85%, 100% {
+    opacity: .99;
+    text-shadow:
+      0 0 8px gold,
+      0 0 44px whitesmoke,
+      0 0 88px navajowhite;
+  }
+  9%, 15.999%, 32%, 46.999%, 70%, 72.999%, 75%, 75.999%, 84%, 84.999% {
+    opacity: .55;
+    text-shadow: none;
+  }
+`
+
+// const hover = keyframes`
+//   from {
+//     transform: skew(-15deg);
+//   }
+//
+//   to {
+//     transform: skew(15deg);
+//   }
+// `
+
+const addFlickeringCss = css`
+  & li {
+    text-shadow:
+      0 0 8px gold,
+      0 0 44px whitesmoke,
+      0 0 88px navajowhite;
+    /* infinity sign */
+    &:nth-of-type(3) {
+      transform: scale(3) rotate(90deg) translateX(0.3rem);
+      ${'' /* animation: ${hover} 5s ease-in-out 2s infinite; */}
+    }
+  }
+  /* TODO: add varying flickering animation to different letters, not the same one */
+  & span {
+    animation: ${flicker} 15s ${HEADER_TIMING}s linear infinite;
+  }
+`
 
 const IndexPage = () => {
-  const [slogan, setSlogan] = useState([`††††††`, `∑∑∑∑∑`, `ΩΩΩΩΩΩ`, `ßßßßßßß`])
+  const [slogan, setSlogan] = useState([`††††††`, `∑∑∑∑∑`, `#`, `ΩΩΩΩΩΩ`, `ßßßßßßß`])
   const [isSloganDecoded, setIsSloganDecoded] = useState(false)
-  const sloganRef = useRef(null)
-  const headerRef = useRef(null)
+  const [isHeaderReady, setIsHeaderReady] = useState(false)
 
   // decode slogan
   useEffect(() => {
-    const timerId = setTimeout(() => {
-      const sloganAdjusted = slogan.map((word, i) => (
-        word.split(``).map((char, j) => {
-          const letter = words[i][j]
-          if (char === letter) return char
+    if (isHeaderReady) {
+      const timerId = setTimeout(() => {
+        const sloganAdjusted = slogan.map((word, i) => (
+          word.split(``).map((char, j) => {
+            const letter = words[i][j]
+            if (char === letter) return char
 
-          const symbolsAndLetter = symbols + letter
-          const pick = symbolsAndLetter[randIdx(symbolsAndLetter)]
-          // Account for the possibility of picking the same symbol twice.
-          // If pick is the same, slip in a letter.
-          if (pick === char) return letter
+            const symbolsAndLetter = symbols + letter
+            const pick = symbolsAndLetter[randIdx(symbolsAndLetter)]
+            // Account for the possibility of picking the same symbol twice.
+            // If pick is the same, slip in a letter.
+            if (pick === char) return letter
 
-          return pick
-        }).join(``)
-      ))
+            return pick
+          }).join(``)
+        ))
 
-      setSlogan(sloganAdjusted)
-    }, 10)
+        setSlogan(sloganAdjusted)
+      }, 50)
 
-    if (slogan.every((el, i) => el === words[i])) {
-      clearTimeout(timerId)
-      setIsSloganDecoded(true)
+      if (slogan.every((el, i) => el === words[i])) {
+        clearTimeout(timerId)
+        setIsSloganDecoded(true)
+      }
     }
-  }, [slogan])
-
-  useEffect(() => {
-    // rearrange slogan - put it into header and form a line instead of a list
-    if (isSloganDecoded) {
-      const scale = 0.25
-      const totalLis = sloganRef.current.childNodes.length
-      const liOriginalHeight = sloganRef.current.childNodes[0].offsetHeight
-      const liEventualHeight = liOriginalHeight * scale
-      const headerHeight = liOriginalHeight / 2
-      const desiredOffsetYPx = (headerHeight - liEventualHeight) / 2
-      const baseline = desiredOffsetYPx * 100 / liOriginalHeight
-
-      headerRef.current.style.height = `${headerHeight}px`
-      sloganRef.current.style.height = `${headerHeight}px`
-
-      const targetYForRest = 100 + (100 / (totalLis - 1))
-      Array.from(sloganRef.current.childNodes).forEach((li, idx) => {
-        const x = (idx >= 2)
-          ? 28 * idx + 10 // make room for infinity sign in the center
-          : 28 * idx
-        const y = (idx === 0)
-          ? baseline
-          // Since values are stacked in a list, we have to lift them up to the
-          // level of the first element. Multiply targetYForRest with respect to
-          // word's position in the list and account for the baseline.
-          : -((targetYForRest * idx) - baseline)
-
-        li.style.transform = `scale(${scale})`
-        li.style.transformOrigin = `${x}% ${y}%`
-      })
-    }
-  }, [isSloganDecoded])
+  }, [isHeaderReady, slogan])
 
   return (
-    <div css={container}>
+    <div css={containerCss}>
       <header
-        css={header}
-        ref={headerRef}
+        css={headerCss}
+        onAnimationStart={() => setIsHeaderReady(true)}
       >
-        <span
-          css={[infinity, isSloganDecoded ? css`margin-top: 0;` : null]}
-        >
-          8
-        </span>
         <ul
-          css={undecoded}
-          ref={sloganRef}
+          css={[sloganCss, isSloganDecoded ? addFlickeringCss : null]}
         >
-          {slogan.map((word) => (
-            <li key={word}>
-              {word}
+          {slogan.map((part) => (
+            <li key={part}>
+              {part.split(``).map((symbol) => (
+                symbol.toLowerCase() === `c` || symbol.toLowerCase() === `t`
+                  ? <span key={part+symbol}>{symbol}</span>
+                  : symbol
+              ))}
             </li>
           ))}
         </ul>
 
         {/* <Link to="/cmd/">Switch to terminal</Link> */}
       </header>
+      <main>
+        A
+      </main>
     </div>
   )
 }

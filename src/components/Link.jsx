3023e36aeca07135c84df1e3c6ed9e7cbd6390c0
Paul Ermolin <oatto.paul@gmail.com>
2019-03-22 13:59:21 +0100

    feat: custom Link component

---
+++ src/components/Link.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link as GatsbyLink } from 'gatsby'
+import PropTypes from 'prop-types'
+
+const Link = ({
+  to, children, cmd, ...other
+}) => (
+  cmd ? (
+    <button type="button" {...other}>
+      {children}
+    </button>
+  ) : (
+    <GatsbyLink
+      to={to}
+      {...other}
+    >
+      {children}
+    </GatsbyLink>
+  )
+)
+
+Link.propTypes = {
+  to: PropTypes.string,
+  children: PropTypes.element,
+  cmd: PropTypes.bool,
+}
+
+Link.defaultProps = {
+  to: `/`,
+  children: `This is nothing but a placholder link text`,
+  cmd: false,
+}
+
+export default Link

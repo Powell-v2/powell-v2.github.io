d7ee6738c2e1814f5ec3044b8744bffc11e9e5a0
Paul Ermolin <oatto.paul@gmail.com>
2019-03-24 20:15:08 +0100

    style(cube): refactor

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -4,9 +4,10 @@ import { css, keyframes } from '@emotion/core'
 
 const containerCss = css`
   display: grid;
+  overflow-y: hidden;
+  place-items: center;
 `
-
-const rotate = keyframes`
+const float = keyframes`
   from, to {
     transform: rotate3d(1, 1, 0, 55deg);
   }
@@ -14,97 +15,110 @@ const rotate = keyframes`
   25% {
     transform:
       rotate3d(1.1, 0.95, 0.05, 57deg)
-      scale3d(1.01, 1.02, 1.03)
-      translate3d(10px, 10px, -10px);
+      scale3d(1.02, 1.02, 1.02)
+      translate3d(10px, 20px, -10px);
   }
 
   60% {
     transform:
       rotate3d(1, 0.85, 0.1, 55deg)
-      scale3d(1.03, 1.04, 1.05)
-      translate3d(-10px, 10px, 10px);
+      scale3d(1.04, 1.04, 1.04)
+      translate3d(-10px, 20px, 10px);
   }
 `
-
 const cubeCss = css`
-  margin: auto;
-  width: 300px;
-  height: 300px;
+  margin-bottom: 2.5rem;
+  width: 25rem;
+  height: 25rem;
   transform-style: preserve-3d;
-  animation: ${rotate} 15s linear 0s infinite;
-  /* facet */
-  & figure {
-    border: 1px solid white;
-    font-size: 3rem;
-    position: absolute;
-    height: 300px;
-    line-height: 300px;
-    width: 300px;
-    margin: 0;
-    text-align: center;
-    & a {
-      color: white;
+  animation: ${float} 15s linear infinite;
+`
+const facetCss = css`
+  border: 1px solid white;
+  font-size: 3rem;
+  position: absolute;
+  height: 25rem;
+  line-height: 25rem;
+  width: 25rem;
+  margin: 0;
+  text-align: center;
+  & a, & p {
+    color: white;
+  }
+  /* front */
+  &:nth-of-type(1) {
+    box-shadow: 0 0 100px rgba(210,0,210,1);
+    background-color: rgba(210,0,210,1);
+    transform: translateZ(12.5rem);
+    &:hover ~ figure:nth-of-type(2) {
+      transform: translateZ(-30rem);
     }
-    /* front */
-    &:nth-of-type(1) {
-      box-shadow: 0 0 100px rgba(210,0,210,1);
-      background-color: rgba(210,0,210,1);
-      transform: translateZ(150px);
-
+  }
+  /* back */
+  &:nth-of-type(2) {
+    background-color: rgba(0,0,210,.7);
+    transform: translateZ(-12.5rem);
+    transition: transform .5s ease-out;
+  }
+  /* right */
+  &:nth-of-type(3) {
+    background-color: rgba(210,0,0,.7);
+    transform: rotateY(90deg) translateZ(12.5rem);
+    transition: transform .5s ease-out;
+  }
+  /* left */
+  &:nth-of-type(4) {
+    box-shadow: 0 0 100px rgba(0,210,210,1);
+    background-color: rgba(0,210,210,1);
+    transform: rotateY(270deg) translateZ(12.5rem);
+    &:hover ~ figure:nth-of-type(3) {
+      transform: rotateY(270deg) translateZ(30rem);
     }
-    /* back */
-    &:nth-of-type(2) {
-      background-color: rgba(0,0,210,.7);
-      transform: rotateY(180deg) translateZ(150px);
-    }
-    /* right */
-    &:nth-of-type(3) {
-      background-color: rgba(210,0,0,.7);
-      transform: rotateY(90deg) translateZ(150px);
-    }
-    /* left */
-    &:nth-of-type(4) {
-      box-shadow: 0 0 100px rgba(0,210,210,1);
-      background-color: rgba(0,210,210,1);
-      transform: rotateY(270deg) translateZ(150px);
-    }
-    /* top */
-    &:nth-of-type(5) {
-      background-color: rgba(210,210,0,.7);
-      transform: rotateX(90deg) translateZ(150px);
-    }
-    /* bottom */
-    &:nth-of-type(6) {
-      box-shadow: 0 0 100px rgba(210,210,210,1);
-      background-color: rgba(210,210,210,1);
-      transform: rotateX(270deg) translateZ(150px);
-    }
+  }
+  /* top */
+  &:nth-of-type(5) {
+    background-color: rgba(210,210,0,.7);
+    transform: rotateX(90deg) translateZ(12.5rem);
+  }
+  /* bottom */
+  &:nth-of-type(6) {
+    box-shadow: 0 0 100px rgba(210,210,210,1);
+    background-color: rgba(210,210,210,1);
+    transform: rotateX(270deg) translateZ(12.5rem);
   }
 `
 
-const links = [
+const notations = [
   // front
-  { name: `About`, to: `/about` },
+  { text: `About`, to: `/about` },
   // back
-  { name: `Jurrasic`, to: `/404` },
+  { text: `Get to know me` },
   // right
-  { name: `Jurrasic`, to: `/404` },
+  { text: `Something to read` },
   // left
-  { name: `Blog`, to: `/blog` },
+  { text: `Blog`, to: `/blog` },
   // top
-  { name: `Jurrasic`, to: `/404` },
+  { text: `Terminal view` },
   // bottom
-  { name: `Switch to cmd`, to: `/cmd` },
+  { text: `Switch to cmd`, to: `/cmd` },
 ]
 
 const Cube = () => (
   <div css={containerCss}>
     <div css={cubeCss}>
-      {links.map(({ name, to }) => (
-        <figure key={Math.random()}>
-          <Link to={to}>
-            {name}
-          </Link>
+      {notations.map(({ text, to }) => (
+        <figure
+          key={Math.random()}
+          css={facetCss}
+        >
+          {to
+            ? (
+              <Link to={to}>
+                {text}
+              </Link>
+            ) : (
+              <p>{text}</p>
+            )}
         </figure>
       ))}
     </div>

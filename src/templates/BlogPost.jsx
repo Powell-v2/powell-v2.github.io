fb5ca22ea2edcf66490a572ae04f4a8d2852e80b
Paul Ermolin <oatto.paul@gmail.com>
2019-03-26 15:57:06 +0100

    style(blog): lay out individual post page

--- src/templates/BlogPost.jsx
+++ src/templates/BlogPost.jsx
@@ -1,21 +1,64 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import PropTypes from 'prop-types'
+import { Global, css } from '@emotion/core'
+
+import Menu from '../components/Menu'
+
+import globalStyles from '../styles/global'
+
+const wrapperCss = css`
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+  grid-template-rows: repeat(2, max-content) auto;
+  grid-template-areas:
+    ". hd      ."
+    ". details ."
+    ". body    .";
+  min-height: 100vh;
+  font-family: "Special Elite";
+  letter-spacing: .05rem;
+  color: whitesmoke;
+  background-color: black;
+`
+const headerCss = css`
+  grid-area: hd;
+  justify-self: center;
+`
+const detailsCss = css`
+  grid-area: details;
+  padding: 2rem 0;
+`
+const bodyCss = css`
+  grid-area: body;
+  padding-bottom: 3rem;
+  & p {
+    padding: 1rem 0;
+    line-height: 1.6;
+  }
+`
 
 const BlogPost = ({ data }) => {
   const { html, frontmatter } = data.markdownRemark
 
   return (
     <>
-      <h1>{frontmatter.title}</h1>
-      <span>
-        Date:
-        {' '}
-        {frontmatter.date}
-      </span>
-      <article
-        dangerouslySetInnerHTML={{ __html: html }}
-      />
+      <Global styles={globalStyles} />
+      <Menu />
+      <section css={wrapperCss}>
+        <h1 css={headerCss}>
+          {frontmatter.title}
+        </h1>
+        <span css={detailsCss}>
+          Date:
+          {' '}
+          {frontmatter.date}
+        </span>
+        <article
+          css={bodyCss}
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </section>
     </>
   )
 }

1155d2a03ad35907f9fecc9bdf47c27a635915b7
Paul Ermolin <oatto.paul@gmail.com>
2019-04-03 20:13:57 +0200

    refactor(terminal): use ContactLinks comp

--- src/components/cmd/content/ContactDetails.jsx
+++ src/components/cmd/content/ContactDetails.jsx
@@ -1,44 +1,22 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPaperPlane, faEnvelope } from '@fortawesome/free-regular-svg-icons'
-import { faGithub, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
+import { css } from '@emotion/core'
 
-const mediums = [
-  {
-    icon: <FontAwesomeIcon icon={faGithub} />,
-    href: `https://github.com/Powell-v2`,
-    name: `GitHub`,
-  },
-  {
-    icon: <FontAwesomeIcon icon={faStackOverflow} />,
-    href: `http://stackoverflow.com/story/powell-v2`,
-    name: `StackOverflow`,
-  },
-  {
-    icon: <FontAwesomeIcon icon={faPaperPlane} />,
-    href: `https://t.me/keepiteasy`,
-    name: `Telegram`,
-  },
-  {
-    icon: <FontAwesomeIcon icon={faEnvelope} />,
-    href: `mailto:ermolinpv@gmail.com`,
-    name: `Email`,
-  },
-]
+import ContactLinks from '../../ContactLinks'
+
+import { palette } from '../../../styles/meta'
+
+const linkStyle = css`
+  color: ${palette.black};
+`
 
 const ContactDetails = () => (
   <article className="output--block">
     <p>Get in touch:</p>
-    <ul className="fa-ul">
-      {mediums.map(({ icon, href, name }) => (
-        <li key={name}>
-          <span className="fa-li">{icon}</span>
-          <a href={href} target={name === `Email` ? null : `_blank`}>
-            {name}
-          </a>
-        </li>
-      ))}
-    </ul>
+    <ContactLinks
+      displayName
+      pullIcon="left"
+      linkStyle={[linkStyle]}
+    />
   </article>
 )
 

e32f7c12a008ca8055acfb4916c225361753eeed
Paul Ermolin <oatto.paul@gmail.com>
2019-04-09 22:04:53 +0100

    Updates

--- src/utils/index.js
+++
@@ -1,5 +0,0 @@
-const randIdx = ({ length }) => Math.floor(Math.random() * length)
-
-const randInt = () => Math.random() * 100000
-
-export { randIdx, randInt }
---
+++ styles-738b21547d150dd79adb.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{110:function(n,o,w){},111:function(n,o,w){}}]);
+//# sourceMappingURL=styles-738b21547d150dd79adb.js.map

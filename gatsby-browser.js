fd7d54858ddf898f79fab6bfe8660eb1074cb02e
Paul Ermolin <oatto.paul@gmail.com>
2019-04-23 14:08:56 +0100

    feat(blog): arrange for highlighting code blocks
    Plugin => prism-react-renderer

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,7 @@
 import React from "react"
 import { MDXProvider } from "@mdx-js/react"
 import { css } from "@emotion/core"
+import Highlight, { defaultProps } from "prism-react-renderer";
 
 import { AppContextProvider } from './src/context/AppContext'
 
@@ -22,7 +23,39 @@ const components = {
       ]}
       {...props}
     />
-  )
+  ),
+  pre: ({ children: { props }, ...other }) => {
+    return (
+      <Highlight
+        language={props.className && props.className.split(`-`)[1]}
+        code={props.children}
+        {...defaultProps}
+        {...other}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre
+            css={css`
+              padding: 2rem;
+              margin: 2rem auto;
+            `}
+            className={className}
+            style={style}
+          >
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => {
+                  console.log({token, key})
+                  return (
+                    token.content && <span {...getTokenProps({ token, key })} />
+                  )
+                })}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+    )
+  }
 }
 
 export const wrapRootElement = ({ element }) => (

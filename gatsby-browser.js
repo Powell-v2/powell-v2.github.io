e2dcba6fb4ca80ef84dca5daf8b8a44e6a1e6ceb
Paul Ermolin <oatto.paul@gmail.com>
2019-04-23 11:30:22 +0100

    feat(blog): use Link in place of anchors in blogposts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,34 @@
 /* eslint-disable */
 import React from "react"
+import { MDXProvider } from "@mdx-js/react"
+import { css } from "@emotion/core"
 
 import { AppContextProvider } from './src/context/AppContext'
 
+import Link from './src/components/Link'
+
 import 'normalize.css/normalize.css'
 import './src/styles/meta.css'
+import { linkHighlighted } from './src/styles/shared'
+
+const components = {
+  a: (props) => (
+    <Link
+      css={[
+        linkHighlighted,
+        css`
+          &:hover { background-color: transparent; }
+        `,
+      ]}
+      {...props}
+    />
+  )
+}
 
 export const wrapRootElement = ({ element }) => (
   <AppContextProvider>
-    {element}
+    <MDXProvider components={components}>
+      {element}
+    </MDXProvider>
   </AppContextProvider>
 )

7b8b38836b0b60301b21cd6d73dee555d7818ae9
Paul Ermolin <oatto.paul@gmail.com>
2019-06-07 19:38:09 +0200

    feat: self-host fonts

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,5 +1,12 @@
 import { css } from '@emotion/core'
 
+import audioWideReg from './fonts/audiowide-regular.woff'
+import audioWideReg2 from './fonts/audiowide-regular.woff2'
+import quicksandReg from './fonts/quicksand-regular.woff'
+import quicksandReg2 from './fonts/quicksand-regular.woff2'
+import quicksandBold from './fonts/quicksand-bold.woff'
+import quicksandBold2 from './fonts/quicksand-bold.woff2'
+
 import { palette } from "./meta"
 
 export default css`
@@ -8,6 +15,33 @@ export default css`
     padding: 0;
     border: 0;
     box-sizing: border-box;
+
+    @font-face {
+      font-family: 'Quicksand';
+      font-display: fallback;
+      src: url(${quicksandBold2}) format('woff2'),
+           url(${quicksandBold}) format('woff');
+      font-weight: 700;
+      font-style: normal;
+    }
+
+    @font-face {
+      font-family: 'Quicksand';
+      font-display: fallback;
+      src: url(${quicksandReg2}) format('woff2'),
+           url(${quicksandReg}) format('woff');
+      font-weight: 400;
+      font-style: normal;
+    }
+
+    @font-face {
+      font-family: 'Audiowide';
+      font-display: swap;
+      src: url(${audioWideReg2}) format('woff2'),
+           url(${audioWideReg}) format('woff');
+      font-weight: normal;
+      font-style: normal;
+    }
   }
 
   html {

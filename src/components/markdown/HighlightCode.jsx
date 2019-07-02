9978f474318bec2ae30e6529f056ca94b52492a4
Paul Ermolin <oatto.paul@gmail.com>
2019-07-02 13:03:53 +0200

    fix(blog): enable scrolling when code block doesn't fit the line

--- src/components/markdown/HighlightCode.jsx
+++ src/components/markdown/HighlightCode.jsx
@@ -3,17 +3,18 @@ import PropTypes from "prop-types"
 import { css } from "@emotion/core"
 import Highlight, { defaultProps } from "prism-react-renderer"
 
-const pre = css`
+const preCss = css`
   padding: 2rem;
   margin: 2rem auto;
+  overflow-x: scroll;
 `
 const codeCss = css`
   padding: .25rem .5rem;
 `
-const lineWrapper = css`
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
+const lineWrapperCss = css`
+  & span:last-child {
+    margin-right: 2rem;
+  }
 `
 
 const HighlightCode = ({ language, code, inline }) => {
@@ -47,19 +48,17 @@ const HighlightCode = ({ language, code, inline }) => {
         className, style, tokens, getLineProps, getTokenProps
       }) => (
         <pre
-          css={pre}
+          css={preCss}
           className={className}
           style={style}
         >
-          {tokens.map((line, i) => (
-            <div
-              css={lineWrapper}
-              {...getLineProps({ line, key: i })}
-            >
-              {line.map((token, key) => (
-                token.content && <span {...getTokenProps({ token, key })} />
-              ))}
-            </div>
+          {tokens.map((line, i) => !line[0].empty && (
+          <div
+            css={lineWrapperCss}
+            {...getLineProps({ line, key: i })}
+          >
+            {line.map((token, key) => token.content && <span {...getTokenProps({ token, key })} />)}
+          </div>
           ))}
         </pre>
       )}

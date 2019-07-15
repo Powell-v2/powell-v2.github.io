1744705e2c4b57d48012efdf4136846b864dfa92
Paul Ermolin <oatto.paul@gmail.com>
2019-07-15 16:56:52 +0200

    refactor(seo): add keywords & nitpicks

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -15,7 +15,7 @@ const detailsQuery = graphql`
   }
 `
 
-function SEO({
+export default function SEO({
   description, lang, meta, keywords, title,
 }) {
   return (
@@ -82,7 +82,7 @@ SEO.defaultProps = {
   description: ``,
   lang: `en`,
   meta: [],
-  keywords: [],
+  keywords: [`blog`, `web development`],
 }
 
 SEO.propTypes = {
@@ -92,5 +92,3 @@ SEO.propTypes = {
   keywords: PropTypes.arrayOf(PropTypes.string),
   title: PropTypes.string.isRequired,
 }
-
-export default SEO

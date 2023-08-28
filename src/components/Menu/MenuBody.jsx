913c78288ee10925af9e17ba0de456994eb5f38e
Paul Yermolin <oatto.paul@gmail.com>
2023-08-28 18:13:50 +0100

    style(menu): revamp

--- src/components/Menu/MenuBody.jsx
+++ src/components/Menu/MenuBody.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import { css } from '@emotion/core'
 
 import Link from '../Link'
 import { useLockBodyScroll } from "../custom_hooks/useLockBodyScroll"
 import ContactLinks from '../ContactLinks'
-import { randInt } from '../../utils'
-import { palette } from '../../styles/meta'
 
 import styles from './menu_body.module.css'
 
@@ -28,87 +25,25 @@ const pages = [
   },
 ]
 
-const menuList = css`
-  display: grid;
-  @media (max-width: 375px) {
-    transform: scale(.8);
-  }
-  @media (min-width: 375px) and (max-width: 500px) {
-    transform: scale(.9);
-  }
-`
-const menuItem = css`
-  height: 4rem;
-  line-height: 4rem;
-  margin: 2rem auto;
-  padding: 0 1.5rem;
-  font-size: 2.5rem;
-  cursor: pointer;
-  text-transform: uppercase;
-`
-const expandOnHover = css`
-  &:hover span {
-    width: 4.5rem;
-    margin: 0 .5rem;
-  }
-`
-const letter = css`
-  display: inline-block;
-  line-height: 4rem;
-  width: 4rem;
-  margin-left: -1rem;
-  background-color: ${palette.purple};
-  text-align: center;
-  vertical-align: middle;
-  transition:
-    width .2s,
-    margin .2s;
-  &:first-of-type {
-    border-top-left-radius: .5rem;
-    border-bottom-left-radius: .5rem;
-  }
-  &:last-of-type {
-    border-top-right-radius: .5rem;
-    border-bottom-right-radius: .5rem;
-  }
-`
-const link = css`
-  color: ${palette.beige};
-  display: inline-block;
-`
-
 const MenuBody = () => {
   useLockBodyScroll()
 
   return (
     <section className={styles.menuContainer}>
-      <nav>
-        <ul css={menuList}>
-          {pages.map(({ name, to }) => (
-            <li
-              key={name}
-              css={[menuItem, !navigator.maxTouchPoints && expandOnHover]}
+      <ul className={styles.pageLinksListWrapper}>
+        {pages.map(({ name, to }) => (
+          <li
+            key={name}
+            className={styles.pageLinkItem}
+          >
+            <Link
+              to={to}
             >
-              <Link
-                to={to}
-                css={link}
-              >
-                {name
-                  .split(``)
-                  .map((char) => (
-                    <span
-                      key={randInt()}
-                      css={letter}
-                    >
-                      {char}
-                    </span>
-                  ))
-                }
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+              {name}
+            </Link>
+          </li>
+        ))}
+      </ul>
       <section className={styles.contactSection}>
         <ContactLinks
           iconsOnly

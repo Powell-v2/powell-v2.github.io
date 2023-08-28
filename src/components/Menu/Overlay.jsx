518e34673b47b11c3847cdf482591f6ef4a442f8
Paul Yermolin <oatto.paul@gmail.com>
2023-08-28 16:28:27 +0100

    refactor(overlay): switch to CSS modules

--- src/components/Menu/Overlay.jsx
+++ src/components/Menu/Overlay.jsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import { css } from '@emotion/core'
 
-const overlay = css`
-  position: fixed;
-  top: 0;
-  left: 0;
-  height: 100vh;
-  width: 100vw;
-  z-index: 12;
-  background-color: rgba(0,0,0,0.93);
-`
+import styles from './overlay.module.css'
 
 const Overlay = () => (
-  <div css={overlay} />
+  <div className={styles.overlay} />
 )
 
 export default Overlay

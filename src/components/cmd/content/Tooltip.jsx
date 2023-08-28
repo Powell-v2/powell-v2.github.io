e8fe1c92a6cd45dcdf0fcb9677fee224dd8a701e
Paul Yermolin <oatto.paul@gmail.com>
2023-08-28 11:37:01 +0100

    feat(cmd): display ASCII art

--- src/components/cmd/content/Tooltip.jsx
+++ src/components/cmd/content/Tooltip.jsx
@@ -1,9 +1,23 @@
+/* eslint-disable no-useless-escape */
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 
+const powellASCII = String.raw`
+   _______    ______    __   __  ___   _______  ___      ___       
+  |   __ "\  /    " \  |"  |/  \|  "| /"     "||"  |    |"  |      
+  (. |__) :)// ____  \ |'  /    \:  |(: ______)||  |    ||  |      
+  |:  ____//  /    ) :)|: /'        | \/    |  |:  |    |:  |      
+  (|  /   (: (____/ //  \//  /\'    | // ___)_  \  |___  \  |___   
+ /|__/ \   \        /   /   /  \\   |(:      "|( \_|:  \( \_|:  \  
+(_______)   \"_____/   |___/    \___| \_______) \_______)\_______) 
+`
+
 const Tooltip = () => (
   <article className="output--block tooltip">
+    {powellASCII.split('\n').filter(Boolean).map((line) => (
+      <pre>{line}</pre>
+    ))}
     <p>
       <FontAwesomeIcon icon={faInfoCircle} />
       {` `}
